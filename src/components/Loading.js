1b836aca1f1c75791c1a4bccc6ee2089540fcb29
Joshua Tan <josh.tan.tann@gmail.com>
2020-11-05 23:52:40 -0800

    Implement live dashboard updating
    Signed-off-by: Joshua Tan <josh.tan.tann@gmail.com>

---
+++ src/components/Loading.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+import classnames from "classnames";
+
+export default class Loading extends Component {
+
+  render() {
+    return <section className="loading">Loading</section>;
+  }
+}
